7ca5922b085717730ba22b38a8d0318c5ec33909
Peter Teuben <teuben@gmail.com>
2023-01-18 10:55:28 -0500

    add 4 more sources

--- mk_runs.py
+++ mk_runs.py
@@ -14,9 +14,11 @@ project="linecheck"
 on = {}
 on['I10565'] = [103436, 103437,  103439, 103440]
 
-on['I05189'] = [103372, 103373, 103380, 103381, 103418, 103419]
+on['I05189'] = [103372, 103373, 103380, 103381, 103418, 103419, 103509, 103510]
 
 on['I12112'] = [103350, 103351]
+
+on['VIIZw31'] = [103505, 103506]
 
 
 #       common parameters per source on the first dryrun (run1, run2)
@@ -25,12 +27,14 @@ pars1 = {}
 pars1['I10565']     = "xlines=110.51,0.15,108.65,0.3,85.2,0.4"
 pars1['I05189']     = "xlines=107.6,0.3"    # needs accuracy
 pars1['I12112']     = "xlines=107.40,0.25"
+pars1['VIIZw31']    = ""
 
 #        common parameters per source on subsequent runs (run1a, run2a)
 pars2 = {}
 pars2['I10565']     = ""
 pars2['I05189']     = ""
 pars2['I12112']     = ""
+pars2['VIIZw31']    = ""
 
 # below here no need to change code
 # ========================================================================
